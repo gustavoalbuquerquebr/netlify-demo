19491596b5a7fe3e0366eca263f6a648e552833c
Gustavo Albuquerque <gustavoalbuquerquebr@gmail.com>
2019-10-07 23:09:36 -0300

    fourth

--- backup/src/index.js
+++ backup/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
 const App = () => (
   <>
@@ -9,3 +12,5 @@ const App = () => (
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// serviceWorker.unregister();
